72b8f33fbdd12fbd69528d2adff5f1a983706301
Lako <Lako>
2020-09-07 14:13:56 -0400

    Add project files.

---
+++ OpenFK/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace OpenFK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axShockwaveFlash1 = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axShockwaveFlash1
+            // 
+            this.axShockwaveFlash1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axShockwaveFlash1.Enabled = true;
+            this.axShockwaveFlash1.Location = new System.Drawing.Point(0, 0);
+            this.axShockwaveFlash1.Name = "axShockwaveFlash1";
+            this.axShockwaveFlash1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash1.OcxState")));
+            this.axShockwaveFlash1.Size = new System.Drawing.Size(800, 600);
+            this.axShockwaveFlash1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.axShockwaveFlash1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "OpenFK";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+    }
+}
+
---
+++ OpenFK/Form1.cs
@@ -0,0 +1,130 @@
+﻿using AxShockwaveFlashObjects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace OpenFK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axShockwaveFlash1.Movie = Directory.GetCurrentDirectory() + @"\Main.swf"; //Sets Main.swf as the Flash Movie to Play.
+            axShockwaveFlash1.Play(); //Plays Main.swf
+            Debug.WriteLine("Main.swf is Loaded");
+            axShockwaveFlash1.FSCommand += new _IShockwaveFlashEvents_FSCommandEventHandler(flashPlayer_FSCommand); //This sets up the FSCommand handler, which CCommunicator likes to use a lot.
+        }
+        void flashPlayer_FSCommand(object sender, _IShockwaveFlashEvents_FSCommandEvent e) //FSCommand Handler
+        {
+            Debug.WriteLine("NEW COMMAND!" + " - " + e.command + " " + e.args);
+            if (e.args.Contains("<load ")) //RDF loading
+            {
+                //Removes Commands section
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(e.args.ToString());
+                var nodeList = doc.SelectNodes("commands");
+                string commandLimited = "";
+                foreach (XmlNode node in nodeList)
+                {
+                    Console.WriteLine(node.InnerXml);
+                    commandLimited = node.InnerXml;
+                }
+                XmlDocument doc2 = new XmlDocument();
+                doc2.LoadXml(commandLimited);
+                XmlElement root = doc2.DocumentElement;
+                //Checks filename.
+                string name = root.GetAttribute("section");
+                //Gets filename
+                string index = File.ReadAllText(@"C:\Users\Lako\Documents\UB Funkeys\RadicaGame\data\system\" + name + ".xml");
+                Debug.WriteLine("RDF - Sent " + name + ".xml");
+                //Sends filename to the game
+                axShockwaveFlash1.SetVariable("msg", index.ToString());
+            }
+            if (e.args.Contains("<save")) //Save
+            {
+                //Removes the Commands Section
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(e.args.ToString());
+                var nodeList = doc.SelectNodes("commands");
+                string commandLimited = "";
+                foreach (XmlNode node in nodeList)
+                {
+                    Console.WriteLine(node.InnerXml);
+                    commandLimited = node.InnerXml;
+                }
+                //Removes the Save section
+                XmlDocument doc2 = new XmlDocument();
+                doc2.LoadXml(commandLimited);
+                XmlElement root = doc2.DocumentElement;
+                //Gets the filename to save.
+                string name = root.GetAttribute("section");
+                var nodeList2 = doc.SelectNodes("save");
+                string commandLimited2 = "";
+                foreach (XmlNode node in nodeList2)
+                {
+                    Console.WriteLine(node.InnerXml);
+                    commandLimited2 = node.InnerXml;
+                }
+                XmlDocument doc3 = new XmlDocument();
+                doc3.LoadXml(commandLimited2);
+                //Saves XML
+                File.WriteAllText(@"C:\Users\Lako\Documents\UB Funkeys\RadicaGame\data\system\" + name + ".xml", commandLimited2);
+                Debug.WriteLine("RDF - Saved " + name + ".xml");
+                axShockwaveFlash1.SetVariable("msg", @"<save name=""system"" section=""" + name + @""" result=""0"" />");
+            }
+            if (e.args.Contains("radicaclose")) //Exit
+            {
+                Application.Exit();
+                Debug.WriteLine("radicaclose called, goodbye!");
+            }
+            if (e.args.Contains("fullscreen")) //Fullscreen
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(e.args.ToString());
+                var nodeList = doc.SelectNodes("commands");
+                string commandLimited = "";
+                foreach (XmlNode node in nodeList)
+                {
+                    Console.WriteLine(node.InnerXml);
+                    commandLimited = node.InnerXml;
+                }
+                XmlDocument doc2 = new XmlDocument();
+                doc2.LoadXml(commandLimited);
+                var nodeList2 = doc2.SelectNodes("state");
+                XmlElement root = doc2.DocumentElement;
+                String name = root.GetAttribute("state");
+                if(name == "1")
+                {
+                    this.FormBorderStyle = FormBorderStyle.None;
+                    this.WindowState = FormWindowState.Maximized;
+                }
+            }
+        }
+        void Form_FormClosing(object sender, FormClosingEventArgs e) //Exit button
+        {
+            e.Cancel = (e.CloseReason == CloseReason.UserClosing);
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                axShockwaveFlash1.SetVariable("msg", @"<radicaclose />");
+            }
+            if (e.CloseReason == CloseReason.WindowsShutDown)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
